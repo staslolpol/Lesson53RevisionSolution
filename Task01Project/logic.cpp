f7edc1afec833236ee60d27fa6e4451ad040441a
staslolpol <staspriho9@gmail.com>
2026-04-06 16:44:44 +0300

    Add project files.

---
+++ Task01Project/logic.cpp
@@ -0,0 +1,16 @@
+﻿// Task 01 [The sum I]
+// Сумма 
+// 
+// Необходимо спроектировать и реализовать программу, 
+// которая вычисляет сумму чисел от 1 до N.
+
+long long sum(int number) {
+    if (number == 0) {
+        return 0;
+    }
+    if (number < 0) {
+        return -1;
+    }
+
+    return number + sum(number - 1);
+}
